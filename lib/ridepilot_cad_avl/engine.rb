284e47e502559ba7ac61adbf2c0863b0f880c499
Xudong Liu <xudong.camsys@gmail.com>
2018-03-22 12:17:04 -0400

    Configure rspec as test framework

--- lib/ridepilot_cad_avl/engine.rb
+++ lib/ridepilot_cad_avl/engine.rb
@@ -5,5 +5,21 @@ require 'fast_jsonapi'
 module RidepilotCadAvl
   class Engine < ::Rails::Engine
     isolate_namespace RidepilotCadAvl
+
+    # Append migrations from the engine into the main app
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
