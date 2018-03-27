2a2da55cd802092e93f4ac8014f2f70eddccd23f
Taylor Liss <tliss@camsys.com>
2018-03-27 17:46:24 -0400

    Refactor code, but map is not rendering

--- app/controllers/ridepilot_cad_avl/cad_controller.rb
+++ app/controllers/ridepilot_cad_avl/cad_controller.rb
@@ -1,6 +1,11 @@
 module RidepilotCadAvl
   class CadController < ApplicationController
+
     def index
+      @provider = current_user.current_provider
+      # current_user.current_provider = @provider
+      # current_user.save!
     end
+
   end
 end
