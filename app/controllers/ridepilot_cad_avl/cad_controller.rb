846059c2646fb715284771e63d6b3fee7e7e755b
Xudong Liu <xudong.camsys@gmail.com>
2018-03-28 11:10:47 -0400

    Update data when cad date changes

--- app/controllers/ridepilot_cad_avl/cad_controller.rb
+++ app/controllers/ridepilot_cad_avl/cad_controller.rb
@@ -3,7 +3,12 @@ module RidepilotCadAvl
     layout "ridepilot_cad_avl/application"
     def index
       @provider = current_user.current_provider
-      @cad_day = Date.today
+      
+      @cad_day = if params[:cad] && !params[:cad][:date].blank?
+        DateTime.parse params[:cad][:date]
+      else
+        Date.today
+      end
 
       @runs = Run.for_provider(current_provider_id).for_date(@cad_day).reorder("lower(name)")
     end
