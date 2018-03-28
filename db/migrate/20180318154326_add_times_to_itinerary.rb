375f9ef43a85eb95ef3fb164e91d0351d10c20e2
Xudong Liu <xudong.camsys@gmail.com>
2018-03-27 22:30:34 -0400

    check column existence

--- db/migrate/20180318154326_add_times_to_itinerary.rb
+++ db/migrate/20180318154326_add_times_to_itinerary.rb
@@ -1,7 +1,15 @@
 class AddTimesToItinerary < ActiveRecord::Migration[5.1]
   def change
-    add_column :itineraries, :departure_time, :datetime
-    add_column :itineraries, :arrival_time, :datetime
-    add_column :itineraries, :finish_time, :datetime
+    unless column_exists? :itineraries, :departure_time
+      add_column :itineraries, :departure_time, :datetime
+    end
+    
+    unless column_exists? :itineraries, :arrival_time
+      add_column :itineraries, :arrival_time, :datetime
+    end
+
+    unless column_exists? :itineraries, :finish_time
+      add_column :itineraries, :finish_time, :datetime
+    end
   end
 end
