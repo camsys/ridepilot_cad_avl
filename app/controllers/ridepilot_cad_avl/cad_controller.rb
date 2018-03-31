cea4465c6efb67996b106eba4b9cc3e359ed6c3b
Xudong Liu <xudong.camsys@gmail.com>
2018-03-31 11:27:49 -0400

    refactor run list and options UI

--- app/controllers/ridepilot_cad_avl/cad_controller.rb
+++ app/controllers/ridepilot_cad_avl/cad_controller.rb
@@ -1,33 +1,34 @@
 module RidepilotCadAvl
   class CadController < ::ApplicationController
     layout "ridepilot_cad_avl/application"
+
+    before_action :get_date
+
     def index
       @provider = current_user.current_provider
-      
-      @cad_day = if params[:cad] && !params[:cad][:date].blank?
-        DateTime.parse params[:cad][:date]
-      else
-        Date.today
-      end
-
       @runs = Run.for_provider(current_provider_id).for_date(@cad_day).reorder("lower(name)")
     end
 
     def reload_runs
-      if !params[:selected_day].blank?
-        @cad_day = DateTime.parse params[:selected_day]
-      end
-
-      if params[:incomplete_runs_only] == "true"
-        @runs = Run.for_provider(current_provider_id).for_date(@cad_day).where(complete: [false, nil]).reorder("lower(name)")
-      else
-        @runs = Run.for_provider(current_provider_id).for_date(@cad_day).reorder("lower(name)")
+      @runs = Run.for_provider(current_provider_id).for_date(@cad_day).reorder("lower(name)")
+      
+      if params[:cad] && params[:cad][:incomplete_runs_only] == 'true'
+        @runs = @runs.where(end_odometer: nil)
       end
 
       respond_to do |format|
         format.js
       end
+    end
 
+    private
+
+    def get_date
+      @cad_day = if params[:cad] && !params[:cad][:date].blank?
+        DateTime.parse params[:cad][:date]
+      else
+        Date.today
+      end
     end
 
   end
