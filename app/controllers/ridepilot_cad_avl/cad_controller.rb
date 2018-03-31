833452932c8c0bec68df33531a9dd9d864f57ea2
Taylor Liss <tliss@camsys.com>
2018-03-30 20:23:34 -0400

    Add route for toggle to reload runs

--- app/controllers/ridepilot_cad_avl/cad_controller.rb
+++ app/controllers/ridepilot_cad_avl/cad_controller.rb
@@ -13,5 +13,10 @@ module RidepilotCadAvl
       @runs = Run.for_provider(current_provider_id).for_date(@cad_day).reorder("lower(name)")
     end
 
+    def reload_runs
+      selected_day = params[:selected_day]
+      incomplete_runs_only = params[:incomplete_runs_only]
+    end
+
   end
 end
