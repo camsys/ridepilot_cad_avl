17f2ee12db8d782ba59c2b932a08c49f31028b19
Xudong Liu <xudong.camsys@gmail.com>
2018-05-04 09:48:06 -0400

    update address serializer

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -2,7 +2,7 @@ class AddressSerializer
   include FastJsonapi::ObjectSerializer
   set_type :address  # optional
 
-  attribute :name, :address, :city, :latitude, :longitude, :notes, :one_line_text
+  attribute :name, :address, :city, :latitude, :longitude, :notes, :address_text
 
   attribute :latlng_only do |object|
     object.coded_by_lat_lng?
